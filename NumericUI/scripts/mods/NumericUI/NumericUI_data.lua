1c03d12589c92060a5fdfcf9df3f2515a47bd965
Dan Reeves <hey@danreev.es>
2023-02-28 02:00:12 +0000

    make numeric ui togglable

--- NumericUI/scripts/mods/NumericUI/NumericUI_data.lua
+++ NumericUI/scripts/mods/NumericUI/NumericUI_data.lua
@@ -1,7 +1,34 @@
 local mod = get_mod("NumericUI")
 
 return {
-    name = "Numeric UI",
-    description = mod:localize("mod_description"),
-    is_togglable = true
+	name = "Numeric UI",
+	description = mod:localize("mod_description"),
+	is_togglable = true,
+	options = {
+		widgets = {
+			{
+				setting_id = "health_text",
+				type = "checkbox",
+				default_value = true,
+			},
+
+			{
+				setting_id = "toughness_text",
+				type = "checkbox",
+				default_value = true,
+			},
+
+			{
+				setting_id = "level",
+				type = "checkbox",
+				default_value = true,
+			},
+
+			{
+				setting_id = "ammo_text",
+				type = "checkbox",
+				default_value = true,
+			},
+		},
+	},
 }
--- NumericUI/scripts/mods/NumericUI/NumericUI_localization.lua
+++ NumericUI/scripts/mods/NumericUI/NumericUI_localization.lua
@@ -1,5 +1,17 @@
 return {
-    mod_description = {
-        en = "Adds numbers to your HUD"
-    }
+	mod_description = {
+		en = "Adds numbers to your HUD",
+	},
+	health_text = {
+		en = "Show health text",
+	},
+	toughness_text = {
+		en = "Show toughness text",
+	},
+	level = {
+		en = "Show level",
+	},
+	ammo_text = {
+		en = "Show ammo text",
+	},
 }
