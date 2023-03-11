516f1d532f2a097c6dff4ed89c2e175f42ba52ff
deluxghost <deluxghost@users.noreply.github.com>
2023-03-11 22:47:43 +0800

    update zh-cn (#16)

--- ChatBlock/scripts/mods/ChatBlock/ChatBlock_localization.lua
+++ ChatBlock/scripts/mods/ChatBlock/ChatBlock_localization.lua
@@ -1,5 +1,6 @@
 return {
 	mod_description = {
 		en = "Automatically hold block while chatting, opening the Steam overlay, or while the game is not focussed",
+		["zh-cn"] = "在打开聊天、打开 Steam 游戏内界面或者切换到其他窗口时自动格挡",
 	},
 }
--- ShowAllBuffs/scripts/mods/ShowAllBuffs/ShowAllBuffs_localization.lua
+++ ShowAllBuffs/scripts/mods/ShowAllBuffs/ShowAllBuffs_localization.lua
@@ -1,5 +1,6 @@
 return {
 	mod_description = {
 		en = "Give blessing buffs icons in the HUD",
+		["zh-cn"] = "在 HUD 上显示祝福效果状态",
 	},
 }
