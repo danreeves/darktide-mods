1bbe26dfe7f20c16829730f2c603f181dd3dd84c
Dan Reeves <hey@danreev.es>
2023-03-12 02:02:58 +0000

    killfeed mod

--- KillfeedImprovements/scripts/mods/KillfeedImprovements/KillfeedImprovements_data.lua
+++ KillfeedImprovements/scripts/mods/KillfeedImprovements/KillfeedImprovements_data.lua
@@ -1,7 +1,39 @@
 local mod = get_mod("KillfeedImprovements")
+local Breeds = require("scripts/settings/breed/breeds")
+
+local breed_toggles = {}
+
+for name, breed in pairs(Breeds) do
+	local tags = breed.tags
+	if tags and (tags.monster or tags.special or tags.elite) then
+		if not string.starts_with(breed.display_name, "unloc") then
+			breed_toggles[#breed_toggles + 1] = {
+				setting_id = name,
+				type = "checkbox",
+				default_value = true,
+			}
+		end
+	end
+end
 
 return {
-	name = "KillfeedImprovements",
+	name = "Killfeed Improvements",
 	description = mod:localize("mod_description"),
 	is_togglable = true,
+	options = {
+		widgets = {
+
+			{
+				setting_id = "include_breeds",
+				type = "group",
+				sub_widgets = breed_toggles,
+			},
+
+			{
+				setting_id = "enable_in_psykanium",
+				type = "checkbox",
+				default_value = false,
+			},
+		},
+	},
 }
--- KillfeedImprovements/scripts/mods/KillfeedImprovements/KillfeedImprovements_localization.lua
+++ KillfeedImprovements/scripts/mods/KillfeedImprovements/KillfeedImprovements_localization.lua
@@ -1,5 +1,24 @@
-return {
+local Breeds = require("scripts/settings/breed/breeds")
+
+local localizations = {
 	mod_description = {
-		en = "KillfeedImprovements description",
+		en = "Deduplicate feed items and filter by breed",
+	},
+	include_breeds = {
+		en = "Show in killfeed",
+	},
+	enable_in_psykanium = {
+		en = "Enable in Psykanium",
 	},
 }
+
+for name, breed in pairs(Breeds) do
+	local tags = breed.tags
+	if tags and (tags.monster or tags.special or tags.elite) then
+		localizations[name] = {
+			en = Localize(breed.display_name),
+		}
+	end
+end
+
+return localizations
