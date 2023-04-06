69d828967cda03f4f3da57ff71e9149cb51f6819
xsSplater <xssplater@gmail.com>
2023-04-06 19:44:34 +0300

    Update PingMonitor_localization.lua

--- PingMonitor/scripts/mods/PingMonitor/PingMonitor_localization.lua
+++ PingMonitor/scripts/mods/PingMonitor/PingMonitor_localization.lua
@@ -1,7 +1,11 @@
 return {
+	mod_name = {
+		en = "Ping Monitor",
+		ru = "Индикатор пинга",
+	},
 	mod_description = {
 		en = "Displays your ping to the host in the tab menu in game",
 		["zh-cn"] = "在游戏内的 Tab 菜单显示到服务器的延迟",
-		ru = "Показывает значения пинга в игре при нажатии ТАВ",
+		ru = "Показывает значение пинга в игре, при нажатии ТАВ",
 	},
 }
