1b84d3b44061c727723193b0aed3476563f3ca6c
xsSplater <xssplater@gmail.com>
2023-04-06 23:44:25 +0300

    Update ShowEquippedInLobby_localization.lua

--- ShowEquippedInLobby/scripts/mods/ShowEquippedInLobby/ShowEquippedInLobby_localization.lua
+++ ShowEquippedInLobby/scripts/mods/ShowEquippedInLobby/ShowEquippedInLobby_localization.lua
@@ -1,18 +1,26 @@
 return {
+	mod_name = {
+		en = "Show Equipped In Lobby",
+		ru = "Показать выбранное оружие в лобби",
+	},
 	mod_description = {
 		en = "Press [E] to Swap Weapon in the Lobby",
 		["zh-cn"] = "按 [E] 在准备界面切换武器显示",
+		ru = "Нажмите [E] для смены оружия в лобби",
 	},
 	default_slot = {
 		en = "Default equipped slot",
 		["zh-cn"] = "默认装备武器",
+		ru = "Выбранный слот по умолчанию",
 	},
 	slot_primary = {
 		en = "Primary",
 		["zh-cn"] = "主武器",
+		ru = "Основное оружие",
 	},
 	slot_secondary = {
 		en = "Secondary",
 		["zh-cn"] = "副武器",
+		ru = "Дополнительное оружие",
 	},
 }
