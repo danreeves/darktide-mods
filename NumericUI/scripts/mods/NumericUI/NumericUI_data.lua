e4c3c50618f898a653ceed30b653da28e21f06d2
xsSplater <xssplater@gmail.com>
2023-04-06 19:35:16 +0300

    NumericUI. Added the possibility of localization of the name.

--- NumericUI/scripts/mods/NumericUI/NumericUI_data.lua
+++ NumericUI/scripts/mods/NumericUI/NumericUI_data.lua
@@ -1,7 +1,7 @@
 local mod = get_mod("NumericUI")
 
 return {
-	name = "Numeric UI",
+	name = mod:localize("mod_name"),
 	description = mod:localize("mod_description"),
 	is_togglable = true,
 	allow_rehooking = true, -- The nameplate require hook needs this
