07bbb28159c1bec44838947371e85d8d8c7746ad
xsSplater <xssplater@gmail.com>
2023-04-06 23:19:15 +0300

    ShowAllBuffs. Added the possibility of localization of the name.

--- ShowAllBuffs/scripts/mods/ShowAllBuffs/ShowAllBuffs_data.lua
+++ ShowAllBuffs/scripts/mods/ShowAllBuffs/ShowAllBuffs_data.lua
@@ -1,7 +1,7 @@
 local mod = get_mod("ShowAllBuffs")
 
 return {
-	name = "Show All Buffs",
+	name = mod:localize("mod_name"),
 	description = mod:localize("mod_description"),
 	is_togglable = true,
 	options = {
