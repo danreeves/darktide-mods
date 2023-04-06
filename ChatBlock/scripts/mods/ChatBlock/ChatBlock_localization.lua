aa8e276f15a5f6f7d6c36b8f3e562fa9b1257475
xsSplater <xssplater@gmail.com>
2023-04-06 10:12:37 +0300

    Chat Block. Added the possibility of localization of the name
    and updated localization.

--- ChatBlock/scripts/mods/ChatBlock/ChatBlock_localization.lua
+++ ChatBlock/scripts/mods/ChatBlock/ChatBlock_localization.lua
@@ -1,7 +1,11 @@
 return {
+	mod_name = {
+		en = "Chat Block",
+		ru = "Чат Блок",
+	},
 	mod_description = {
 		en = "Automatically hold block while chatting, opening the Steam overlay, or while the game is not focussed",
 		["zh-cn"] = "在打开聊天、打开 Steam 游戏内界面或者切换到其他窗口时自动格挡",
-		ru = "Автоматически удерживается блок во время использования чата, при открытии оверлея Steam или когда игра не находится в фокусе.",
+		ru = "Автоматическое удержание блока во время использования чата, при открытии оверлея Steam или когда игра не находится в фокусе.",
 	},
 }
