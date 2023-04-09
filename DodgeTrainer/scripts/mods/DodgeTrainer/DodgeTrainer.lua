0c583d987a21c17023cade7d2f7ab6e7e3442d9c
Dan Reeves <hey@danreev.es>
2023-04-09 13:14:13 +0100

    fix rehooking

--- DodgeTrainer/scripts/mods/DodgeTrainer/DodgeTrainer.lua
+++ DodgeTrainer/scripts/mods/DodgeTrainer/DodgeTrainer.lua
@@ -27,3 +27,32 @@ mod:hook("UIHud", "init", function(func, self, elements, visibility_groups, para
 
 	return func(self, elements, visibility_groups, params)
 end)
+
+mod.time_between = 0
+mod.exit_time = 0
+
+local function on_enter(_self, _unit, _dt, t, _previous_state, _params)
+	mod.time_between = t - mod.exit_time
+	mod.exit_time = 0
+end
+
+local function on_exit(_self, _unit, t, _next_state)
+	mod.exit_time = t
+end
+
+mod:hook_safe("PlayerCharacterStateDodging", "on_enter", on_enter)
+mod:hook_safe("PlayerCharacterStateDodging", "on_exit", on_exit)
+
+mod:hook_safe("PlayerCharacterStateSliding", "on_enter", on_enter)
+mod:hook_safe("PlayerCharacterStateSliding", "on_exit", on_exit)
+
+mod:hook_safe("PlayerCharacterStateSprinting", "on_enter", function(...)
+	if mod:get("include_sprinting") then
+		on_enter(...)
+	end
+end)
+mod:hook_safe("PlayerCharacterStateSprinting", "on_exit", function(...)
+	if mod:get("include_sprinting") then
+		on_exit(...)
+	end
+end)
--- DodgeTrainer/scripts/mods/DodgeTrainer/HudElementDodgeTrainer.lua
+++ DodgeTrainer/scripts/mods/DodgeTrainer/HudElementDodgeTrainer.lua
@@ -20,7 +20,6 @@ local text_style = {
 	line_spacing = 1.2,
 	font_size = 25,
 	drop_shadow = true,
-	font_type = "calibri",
 	font_type = "proxima_nova_bold",
 	text_color = Color.white(255, true),
 	size = size,
@@ -66,41 +65,12 @@ HudElementDodgeTrainer.update = function(self, dt, t, ui_renderer, render_settin
 	local color = Color.lerp(Color.ui_green_light(), Color.ui_red_light(), p)
 	local _, r, g, b = Quaternion.to_elements(color)
 
-	if time_between and time_between <= max_cutoff and p <= 1 and p >= 0 then
+	if time_between > 0 and time_between <= max_cutoff and p <= 1 and p >= 0 then
 		content.text = string.format("%.2fs", mod.time_between)
 		style.text.text_color[2] = r
 		style.text.text_color[3] = g
 		style.text.text_color[4] = b
 	end
 end
-
-mod.time_between = 0
-mod.exit_time = 0
-
-local function on_enter(_self, _unit, _dt, t, _previous_state, _params)
-	mod.time_between = t - mod.exit_time
-	mod.exit_time = 0
-end
-
-local function on_exit(_self, _unit, t, _next_state)
-	mod.exit_time = t
-end
-
-mod:hook_safe("PlayerCharacterStateDodging", "on_enter", on_enter)
-mod:hook_safe("PlayerCharacterStateDodging", "on_exit", on_exit)
-
-mod:hook_safe("PlayerCharacterStateSliding", "on_enter", on_enter)
-mod:hook_safe("PlayerCharacterStateSliding", "on_exit", on_exit)
-
-mod:hook_safe("PlayerCharacterStateSprinting", "on_enter", function(...)
-	if mod:get("include_sprinting") then
-		on_enter(...)
-	end
-end)
-mod:hook_safe("PlayerCharacterStateSprinting", "on_exit", function(...)
-	if mod:get("include_sprinting") then
-		on_exit(...)
-	end
-end)
 
 return HudElementDodgeTrainer
