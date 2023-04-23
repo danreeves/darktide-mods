a51a41b1a8397db6fab25727caaf33b7b2bb2fb5
xsSplater <xssplater@gmail.com>
2023-04-23 15:58:46 +0300

    MuteInBackground RU local (#65)

--- MuteInBackground/scripts/mods/MuteInBackground/MuteInBackground_localization.lua
+++ MuteInBackground/scripts/mods/MuteInBackground/MuteInBackground_localization.lua
@@ -1,5 +1,10 @@
 return {
+	mod_name = {
+		en = "Mute In Background",
+		ru = "Отключение звука игры в фоне",
+	},
 	mod_description = {
-		en = "MuteInBackground description",
+		en = "Mutes the game while it's in the background or not focused",
+		ru = "Mute In Background - Отключает звук игры, когда она находится в фоновом режиме или не в фокусе.",
 	},
 }
