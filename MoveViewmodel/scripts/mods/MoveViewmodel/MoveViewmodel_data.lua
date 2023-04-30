8d07855a0a614bb0b49f8c38876f6a0c2abffae9
Dan Reeves <hey@danreev.es>
2023-04-30 03:34:04 +0100

    move viewmodel mod

--- MoveViewmodel/scripts/mods/MoveViewmodel/MoveViewmodel_data.lua
+++ MoveViewmodel/scripts/mods/MoveViewmodel/MoveViewmodel_data.lua
@@ -4,4 +4,16 @@ return {
 	name = "MoveViewmodel",
 	description = mod:localize("mod_description"),
 	is_togglable = true,
+	options = {
+		widgets = {
+			{
+				setting_id = "open_editor",
+				type = "keybind",
+				default_value = {},
+				keybind_trigger = "pressed",
+				keybind_type = "function_call",
+				function_name = "toggle_editor",
+			},
+		},
+	},
 }
