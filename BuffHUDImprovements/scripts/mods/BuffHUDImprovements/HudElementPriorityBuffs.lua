1b57a690fa3f8dce33f4e731b893de951aea1866
Dan Reeves <hey@danreev.es>
2023-05-06 20:53:11 +0100

    clean up

--- BuffHUDImprovements/scripts/mods/BuffHUDImprovements/HudElementPriorityBuffs.lua
+++ BuffHUDImprovements/scripts/mods/BuffHUDImprovements/HudElementPriorityBuffs.lua
@@ -2,24 +2,8 @@ local mod = get_mod("BuffHUDImprovements")
 
 -- Required to exist before sub-classing it
 require("scripts/ui/hud/elements/player_buffs/hud_element_player_buffs_polling")
-local PlayerBuffDefinitions = require("scripts/ui/hud/elements/player_buffs/hud_element_player_buffs_definitions")
 
 local HudElementPriorityBuffs = class("HudElementPriorityBuffs", "HudElementPlayerBuffs")
-
-function HudElementPriorityBuffs:init(parent, draw_layer, start_scale, definitions)
-	HudElementPriorityBuffs.super.init(self, parent, draw_layer, start_scale, table.clone(PlayerBuffDefinitions))
-
-	self._player = parent:player()
-	self._active_buffs_data = {}
-	self._active_positive_buffs = 0
-	self._active_negative_buffs = 0
-
-	self:_update_buff_alignments(true)
-end
-
-function HudElementPriorityBuffs:update(dt, t, ui_renderer, render_settings, input_service)
-	HudElementPriorityBuffs.super.update(self, dt, t, ui_renderer, render_settings, input_service)
-end
 
 function HudElementPriorityBuffs:event_player_buff_added(player, buff_instance)
 	if not self._player or self._player ~= player then
@@ -31,7 +15,6 @@ function HudElementPriorityBuffs:event_player_buff_added(player, buff_instance)
 	local add_buff = buff_instance:has_hud()
 
 	if add_buff and is_priority then
-		mod:echo("Adding priority buff")
 		self:_add_buff(buff_instance)
 	end
 end
