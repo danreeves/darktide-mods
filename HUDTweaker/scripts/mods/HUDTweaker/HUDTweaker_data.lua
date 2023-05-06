d68a7a8d222bf1a321cdf179729a6e399c28571a
xsSplater <xssplater@gmail.com>
2023-05-06 13:37:59 +0300

    Update HUDTweaker_data.lua

--- HUDTweaker/scripts/mods/HUDTweaker/HUDTweaker_data.lua
+++ HUDTweaker/scripts/mods/HUDTweaker/HUDTweaker_data.lua
@@ -1,7 +1,7 @@
 local mod = get_mod("HUDTweaker")
 
 return {
-	name = "HUD Tweaker",
+	name = mod:localize("mod_name"),
 	description = mod:localize("mod_description"),
 	is_togglable = true,
 	options = {
