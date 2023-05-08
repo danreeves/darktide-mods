a8e5cd0d544caa71c5383f259d66da45dc9c08b0
xsSplater <xssplater@gmail.com>
2023-05-08 22:42:45 +0300

    Update HUDTweaker_localization.lua

--- HUDTweaker/scripts/mods/HUDTweaker/HUDTweaker_localization.lua
+++ HUDTweaker/scripts/mods/HUDTweaker/HUDTweaker_localization.lua
@@ -5,16 +5,12 @@ return {
 	},
 	mod_description = {
 		en = "A tool to tweak the styles of any HUD element",
+		["zh-cn"] = "调整 HUD 元素风格的工具",
+		ru = "HUD Tweaker - Инструмент для настройки стилей любого элемента интерфейса",
 	},
 	toggle_hud_tweaker = {
 		en = "Toggle HUD Tweaker window",
-	},
-	search_box = {
-		en = "Search",
-		ru = "Найти",
-	},
-	reset_tweaks = {
-		en = "Reset tweaks",
-		ru = "Сброс настроек",
+		["zh-cn"] = "开关 HUD 调整窗口",
+		ru = "Клавиша включения/выключения окна настроек",
 	},
 }
