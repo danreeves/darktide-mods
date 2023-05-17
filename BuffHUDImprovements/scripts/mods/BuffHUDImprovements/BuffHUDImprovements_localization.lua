0201f9057f96abb22966faa3f19e6c853572b0ba
xsSplater <xssplater@gmail.com>
2023-05-17 13:29:22 +0300

    Update BuffHUDImprovements_localization.lua (#77)
    Co-authored-by: Dan Reeves <hey@danreev.es>

--- BuffHUDImprovements/scripts/mods/BuffHUDImprovements/BuffHUDImprovements_localization.lua
+++ BuffHUDImprovements/scripts/mods/BuffHUDImprovements/BuffHUDImprovements_localization.lua
@@ -2,21 +2,26 @@ return {
 	mod_name = {
 		en = "Buff HUD Improvements",
 		["zh-cn"] = "状态效果 HUD 优化",
+		ru = "Улучшение интерфейса баффов",
 	},
 	mod_description = {
 		en = "Disable buffs, move important buffs to a new buff bar, a framework for custom buffs",
 		["zh-cn"] = "禁用状态效果，将重要效果移动到单独的状态栏，并可作为自定义状态效果框架",
+		ru = "Buff HUD Improvements - Отключите баффы или переместите важные баффы на новую панель баффов, сделанную для пользовательских баффов.",
 	},
 	open_buff_settings = {
 		en = "Open settings window",
 		["zh-cn"] = "打开设置窗口",
+		ru = "Открыть окно настроек",
 	},
 	custom_buffs = {
 		en = "Custom buffs",
 		["zh-cn"] = "自定义状态效果",
+		ru = "Пользовательские баффы",
 	},
 	custom_toughness_broken_buff = {
 		en = "Toughness broken grace period",
 		["zh-cn"] = "韧性伤害恢复时间",
+		ru = "Откат пробития Стойкости",
 	},
 }
