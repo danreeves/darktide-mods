2c6d60d2f6b3b20beabf91c8e50b22a7997b8b63
xsSplater <xssplater@gmail.com>
2023-05-26 13:34:27 +0300

    Update MultipleKeybinds_localization.lua

--- MultipleKeybinds/scripts/mods/MultipleKeybinds/MultipleKeybinds_localization.lua
+++ MultipleKeybinds/scripts/mods/MultipleKeybinds/MultipleKeybinds_localization.lua
@@ -1,11 +1,14 @@
 return {
 	mod_name = {
 		en = "Multiple Keybinds",
+		ru = "Дополнительные сочетания клавиш",
 	},
 	mod_description = {
 		en = "Allows setting two keybinds per action",
+		ru = "Multiple Keybinds - Разрешает установить два сочетания клавиш на одно действие",
 	},
 	unset_on_rightclick = {
 		en = "Allow unsetting keybinds with right click",
+		ru = "Разрешить очистку сочетаний клавиш правой кнопкой мыши",
 	},
 }
