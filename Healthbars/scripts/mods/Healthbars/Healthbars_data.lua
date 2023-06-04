09226f797c3abe920d6e00ebb4f72f043fc8c164
Dan Reeves <hey@danreev.es>
2023-06-04 17:36:57 +0100

    add support for chaos spawn, refactor mod options

--- Healthbars/scripts/mods/Healthbars/Healthbars_data.lua
+++ Healthbars/scripts/mods/Healthbars/Healthbars_data.lua
@@ -1,20 +1,34 @@
 local mod = get_mod("Healthbars")
 local Breeds = require("scripts/settings/breed/breeds")
 
-local breed_widgets = {}
+local horde_and_roamers = {}
+local elites = {}
+local specials = {}
+local monsters = {}
+
+local function add(tbl, breed_name, default_value)
+	tbl[#tbl + 1] = {
+		setting_id = breed_name,
+		type = "checkbox",
+		default_value = default_value,
+	}
+end
 
 for breed_name, breed in pairs(Breeds) do
-	if breed_name ~= "chaos_spawn" and breed_name ~= "chaos_plague_ogryn_sprayer" then
-		if breed.tags.minion then
-			local default_value = false
-			if breed.tags.elite or breed.tags.special then
-				default_value = true
-			end
-			breed_widgets[#breed_widgets + 1] = {
-				setting_id = breed_name,
-				type = "checkbox",
-				default_value = default_value,
-			}
+	if breed.tags.minion then
+		local default_value = false
+		if breed.tags.elite or breed.tags.special then
+			default_value = true
+		end
+
+		if breed.tags.horde or breed.tags.roamer then
+			add(horde_and_roamers, breed_name, default_value)
+		elseif breed.tags.elite then
+			add(elites, breed_name, default_value)
+		elseif breed.tags.special then
+			add(specials, breed_name, default_value)
+		elseif breed.tags.monster or breed.tags.captain then
+			add(monsters, breed_name, default_value)
 		end
 	end
 end
@@ -47,9 +61,24 @@ local widgets = {
 		},
 	},
 	{
-		setting_id = "toggle_breeds",
+		setting_id = "horde_breeds",
 		type = "group",
-		sub_widgets = breed_widgets,
+		sub_widgets = horde_and_roamers,
+	},
+	{
+		setting_id = "elite_breeds",
+		type = "group",
+		sub_widgets = elites,
+	},
+	{
+		setting_id = "special_breeds",
+		type = "group",
+		sub_widgets = specials,
+	},
+	{
+		setting_id = "monster_breeds",
+		type = "group",
+		sub_widgets = monsters,
 	},
 }
 
