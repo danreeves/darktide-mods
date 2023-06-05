71e7cdabe7ba70b40a794434e573f32761b273dd
deluxghost <deluxghost@users.noreply.github.com>
2023-06-05 11:50:15 +0800

    update zh-cn

--- TrueSoloQoL/scripts/mods/TrueSoloQoL/TrueSoloQoL_localization.lua
+++ TrueSoloQoL/scripts/mods/TrueSoloQoL/TrueSoloQoL_localization.lua
@@ -1,26 +1,34 @@
 return {
 	mod_name = {
 		en = "True Solo QoL",
+		["zh-cn"] = "True Solo 辅助功能",
 	},
 	mod_description = {
 		en = "Disables bots, adds auto-restart, pause, and teleport utilities",
+		["zh-cn"] = "禁用机器人，添加自动重开、暂停和传送功能",
 	},
 	disable_bots = {
 		en = "Disable bots",
+		["zh-cn"] = "禁用机器人",
 	},
 	auto_restart = {
 		en = "Auto restart failed missions",
+		["zh-cn"] = "任务失败自动重开",
 	},
 	restart_level = {
 		en = "Restart the current mission",
+		["zh-cn"] = "重开当前任务",
 	},
 	skip_cutscene = {
 		en = "Skip intro cutscene",
+		["zh-cn"] = "跳过开局过场动画",
 	},
 	toggle_pause = {
 		en = "Toggle gameplay pause",
+		["zh-cn"] = "开关游戏暂停",
 	},
 	teleport_to_cursor = {
 		en = "Teleport to look location",
+		["zh-cn"] = "传送到准星位置",
 	},
 }
