c239451fbc74c8190e4be0ea311aeea337ee1e16
xsSplater <xssplater@gmail.com>
2023-07-22 20:52:48 +0300

    Update MultipleKeybinds_localization.lua (#80)
    Co-authored-by: Dan Reeves <hey@danreev.es>

--- MultipleKeybinds/scripts/mods/MultipleKeybinds/MultipleKeybinds_localization.lua
+++ MultipleKeybinds/scripts/mods/MultipleKeybinds/MultipleKeybinds_localization.lua
@@ -1,14 +1,17 @@
 return {
 	mod_name = {
 		en = "Multiple Keybinds",
+		ru = "Дополнительные сочетания клавиш",
 		["zh-cn"] = "绑定多个快捷键",
 	},
 	mod_description = {
 		en = "Allows setting two keybinds per action",
+		ru = "Multiple Keybinds - Разрешает установить два сочетания клавиш на одно действие",
 		["zh-cn"] = "允许为每个操作设置两个快捷键",
 	},
 	unset_on_rightclick = {
 		en = "Allow unsetting keybinds with right click",
+		ru = "Разрешить очистку сочетаний клавиш правой кнопкой мыши",
 		["zh-cn"] = "允许右键取消快捷键",
 	},
 }
