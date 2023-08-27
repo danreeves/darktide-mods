70c795fdee2cade27a0eb148712d477cf6301826
xsSplater <xssplater@gmail.com>
2023-08-27 23:11:14 +0300

    Update LogMeIn_localization.lua

--- LogMeIn/scripts/mods/LogMeIn/LogMeIn_localization.lua
+++ LogMeIn/scripts/mods/LogMeIn/LogMeIn_localization.lua
@@ -1,14 +1,21 @@
 return {
+	mod_name = {
+		en = "Log Me In",
+		ru = "Впусти меня",
+	},
 	mod_description = {
 		en = "Automatically hits space for you at the title screen",
 		["zh-cn"] = "在标题画面自动按空格",
+		ru = "Log Me In - Автоматически нажимает пробел на титульном экране.",
 	},
 	cancel_auto_character_select = {
 		en = "Press to cancel auto character selection",
 		["zh-cn"] = "按键取消自动选择角色",
+		ru = "Нажмите для отмены автовыбора персонажа",
 	},
 	auto_character_select = {
 		en = "Select your most recently played character automatically",
 		["zh-cn"] = "自动选择最近玩过的角色",
+		ru = "Автовыбор персонажа, которым вы играли в последний раз",
 	},
 }
