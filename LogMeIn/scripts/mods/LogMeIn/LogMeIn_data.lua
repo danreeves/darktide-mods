1ae9d45e4e8664329331e7c309819c3eb3ee79d6
xsSplater <xssplater@gmail.com>
2023-10-04 04:03:24 +0300

    Update LogMeIn_localization.lua (#86)

--- LogMeIn/scripts/mods/LogMeIn/LogMeIn_data.lua
+++ LogMeIn/scripts/mods/LogMeIn/LogMeIn_data.lua
@@ -1,7 +1,7 @@
 local mod = get_mod("LogMeIn")
 
 return {
-	name = "Log Me In",
+	name = mod:localize("mod_name"),
 	description = mod:localize("mod_description"),
 	is_togglable = false,
 	options = {
