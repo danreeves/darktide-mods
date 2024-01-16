a0826a068c30c5335ec4bc9777e8ab5c2bd9527b
Dan Reeves <hey@danreev.es>
2024-01-16 02:28:20 +0000

    new images

--- SanrioBoys/scripts/mods/SanrioBoys/SanrioBoys.lua
+++ SanrioBoys/scripts/mods/SanrioBoys/SanrioBoys.lua
@@ -1,4 +1,7 @@
 local mod = get_mod("SanrioBoys")
+local MissionUtilities = require("scripts/utilities/ui/mission")
+local MissionBoardViewSettings = require("scripts/ui/views/mission_board_view/mission_board_view_settings")
+local MissionTemplates = require("scripts/settings/mission/mission_templates")
 
 local characters = {
 	explicator_a = {
@@ -40,3 +43,72 @@ mod:hook("HudElementMissionSpeakerPopup", "_mission_speaker_start", function(fun
 	end
 	return func(self, _name_text, _icon)
 end)
+
+mod:hook_safe("MissionBoardView", "_fetch_success", function(self, data)
+	local missions = data.missions
+	local narrative_mission = MissionUtilities.get_latest_narrative_mission(missions)
+	local mission_giver = narrative_mission and narrative_mission.missionGiver
+	local character = characters[mission_giver]
+	if character then
+		self._widgets_by_name.story_mission_view_button_frame.style.char.material_values.main_texture =
+			character.texture
+		self._widgets_by_name.story_mission_view_button_frame.style.char.size = {
+			120,
+			120,
+		}
+	end
+end)
+
+mod:hook_safe("MissionBoardView", "_set_selected_quickplay", function(self)
+	local vo_profile = MissionBoardViewSettings.quickplay_vo_profile
+	local character = characters[vo_profile]
+
+	local widget = self._widgets_by_name.objective_1
+	widget.dirty = true
+	widget.visible = true
+	local content = widget.content
+	local style = widget.style
+
+	-- content.speaker_icon = character.textures
+	content.speaker_text = "/ " .. character.name
+
+	if not style.speaker_icon.material_values then
+		style.speaker_icon.material_values = {}
+	end
+	style.speaker_icon.material_values.texture_map = character.texture
+end)
+
+mod:hook_safe("MissionBoardView", "_set_selected_mission", function(self, mission, move_gamepad_cursor, is_flash)
+	local mission_template = MissionTemplates[mission.map]
+	local vo_profile = mission.missionGiver or mission_template.mission_brief_vo.vo_profile
+	local character = characters[vo_profile]
+
+	local widget = self._widgets_by_name.objective_1
+	widget.dirty = true
+	widget.visible = true
+
+	local content = widget.content
+	local style = widget.style
+
+	-- content.speaker_icon = character.material_small
+	content.speaker_text = "/ " .. character.name
+
+	if not style.speaker_icon.material_values then
+		style.speaker_icon.material_values = {}
+	end
+	style.speaker_icon.material_values.texture_map = character.texture
+end)
+
+-- mod:hook_require("scripts/ui/views/mission_board_view/mission_board_view_definitions", function(instance)
+-- 	local objective_1 = instance.widget_definitions.objective_1
+
+-- 	local function _has_speaker(content)
+-- 		return content.speaker_text
+-- 	end
+
+-- 	for _, pass in ipairs(objective_1.passes) do
+-- 		if pass.style_id == "speaker_icon" or pass.style_id == "speaker_text" then
+-- 			pass.visibility_function = _has_speaker
+-- 		end
+-- 	end
+-- end)
