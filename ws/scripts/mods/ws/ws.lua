73eb494a1c4396a404d55bc174e741ec48c4e2d9
Dan Reeves <hey@danreev.es>
2024-07-27 22:01:10 +0100

    text update

--- ws/scripts/mods/ws/ws.lua
+++ ws/scripts/mods/ws/ws.lua
@@ -1,19 +1,19 @@
 local mod = get_mod("ws")
 
 if not DarktideWs then
-	mod:echo("Websocket plugin missing.")
+	mod:echo("darktide_ws_plugin missing.")
 	return
 end
 
-function _darktide_ws_callback(message)
+function _darktide_plugin_ws_on_message(message)
 	mod:echo(message)
 end
 
-mod:hook_safe("MultiplayerSession", "joined_host", function ()
+mod:hook_safe("MultiplayerSession", "joined_host", function()
 	local session_id = tostring(Managers.connection:session_id())
 	DarktideWs.join_room(session_id)
 end)
 
-mod:command("ws", "Websocket commands", function(...) 
-	DarktideWs.send_message(table.concat({...}, " "))
+mod:command("ws", "Websocket commands", function(...)
+	DarktideWs.send_message(table.concat({ ... }, " "))
 end)
