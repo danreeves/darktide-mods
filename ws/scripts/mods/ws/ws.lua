7b230e2462f54a9b08bb900b525346247f6828e0
Dan Reeves <hey@danreev.es>
2024-08-03 03:33:41 +0100

    update for new websocket api

--- ws/scripts/mods/ws/ws.lua
+++ ws/scripts/mods/ws/ws.lua
@@ -1,19 +1,19 @@
 local mod = get_mod("ws")
 
-if not DarktideWs then
+if not WebSockets then
 	mod:echo("darktide_ws_plugin missing.")
 	return
 end
 
-function _darktide_plugin_ws_on_message(message)
+local connection = WebSockets.connect("wss://ws.darkti.de", function(message)
 	mod:echo(message)
-end
+end)
 
 mod:hook_safe("MultiplayerSession", "joined_host", function()
 	local session_id = tostring(Managers.connection:session_id())
-	DarktideWs.join_room(session_id)
+	connection:send(cjson.encode({ type = "join", room = session_id }))
 end)
 
-mod:command("ws", "Websocket commands", function(...)
-	DarktideWs.send_message(table.concat({ ... }, " "))
-end)
+-- mod:command("ws", "Websocket commands", function(...)
+-- 	DarktideWs.send_message(table.concat({ ... }, " "))
+-- end)
