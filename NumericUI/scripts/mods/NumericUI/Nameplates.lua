f1a2b9d80c3ef34d0e44a130f2d3b62c7b1b8eb7
zombine <129126774+zombine04@users.noreply.github.com>
2025-03-29 23:17:05 +0900

    Fixed Class Icons in Nameplates (#110)

--- NumericUI/scripts/mods/NumericUI/Nameplates.lua
+++ NumericUI/scripts/mods/NumericUI/Nameplates.lua
@@ -1,4 +1,5 @@
 local mod = get_mod("NumericUI")
+local UISettings = require("scripts/settings/ui/ui_settings")
 
 mod:hook_require(
 	"scripts/ui/hud/elements/world_markers/templates/world_marker_template_nameplate_combat",
@@ -10,7 +11,8 @@ mod:hook_require(
 				local content = widget.content
 				local profile = data:profile()
 				local archetype = profile and profile.archetype
-				local symbol_string = archetype and archetype.string_symbol or ""
+				local archetype_name = archetype and archetype.name
+				local symbol_string = archetype_name and UISettings.archetype_font_icon[archetype_name] or ""
 				local replace_string = mod:get("color_nameplate") and "{#reset%(%)}" or ""
 
 				content.header_text = content.header_text:gsub(replace_string, symbol_string)
