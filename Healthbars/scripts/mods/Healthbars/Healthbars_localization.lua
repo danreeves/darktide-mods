b5216c906e9f2474fc2f4a73c36c91996f21169f
Dan Reeves <hey@danreev.es>
2025-04-03 21:37:52 +0100

    Revert "Update healthbars based on latest source"
    This reverts commit 1ada88fbe2f00761f440c54eb558fbe74fe92fd4.

--- Healthbars/scripts/mods/Healthbars/Healthbars_localization.lua
+++ Healthbars/scripts/mods/Healthbars/Healthbars_localization.lua
@@ -31,39 +31,10 @@ local localization = {
 		["zh-cn"] = "显示 DPS 报告",
 		ru = "Отчёт Урон в секунду",
 	},
-	show_dps_description = {
-		en = "Requires damage numbers to be enabled",
-		["zh-cn"] = "需要启用伤害数字",
-		ru = "Требуется включение цифр урона",
-	},
 	show_armour_type = {
 		en = "Show armour type hit",
 		["zh-cn"] = "显示命中护甲类型",
 		ru = "Тип поражённой брони",
-	},
-	show_armour_type_description = {
-		en = "Requires damage numbers to be enabled",
-		["zh-cn"] = "需要启用伤害数字",
-		ru = "Требуется включение цифр урона",
-	},
-	damage_number_type = {
-		en = "Visual style of damage numbers",
-		["zh-cn"] = "伤害数字的视觉风格",
-		ru = "Визуальный стиль цифр урона",
-	},
-	readable = {
-		en = "Readable",
-	},
-	floating = {
-		en = "Floating",
-	},
-	flashy = {
-		en = "Flashy",
-	},
-	max_distance = {
-		en = "Max distance",
-		["zh-cn"] = "最大距离",
-		ru = "Максимальное расстояние",
 	},
 	horde_breeds = {
 		en = "Horde/Roamer",
@@ -100,9 +71,6 @@ local localization = {
 for breed_name, breed in pairs(Breeds) do
 	if breed.tags.minion then
 		local display_name = Localize(breed.display_name)
-		if display_name:find("<") and display_name:find(">") then
-			display_name = breed.display_name
-		end
 		localization[breed_name] = {
 			en = "Show " .. display_name .. " health",
 			["zh-cn"] = "显示" .. display_name .. "的血量",
