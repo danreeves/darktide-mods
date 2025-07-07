29a5590454d063ed73e52f063802cdde30309b5d
BoffeeH <han.zl@outlook.com>
2025-07-07 15:52:56 -0300

    support for ritualist and others
    - Enabled healthbar for dreg ritualist (chaos_mutator_ritualist, cultist_ritualist)
    - Enabled healthbar for Admonition Champion (cultist_captain)
    - Added English name for chaos_mutated_poxwalker, chaos_lesser_mutated_poxwalker
    - Renamed mutator enemies for clarity
    - Improved Chinese localization

--- Healthbars/scripts/mods/Healthbars/Healthbars_data.lua
+++ Healthbars/scripts/mods/Healthbars/Healthbars_data.lua
@@ -5,6 +5,7 @@ local horde_and_roamers = {}
 local elites = {}
 local specials = {}
 local monsters = {}
+local ritualists = {}
 
 local function add(tbl, breed_name, default_value)
 	tbl[#tbl + 1] = {
@@ -15,9 +16,17 @@ local function add(tbl, breed_name, default_value)
 end
 
 for breed_name, breed in pairs(Breeds) do
+	-- if breed.tags and breed.tags.minion then
+		-- local tag_list = {}
+		-- for tag, _ in pairs(breed.tags) do
+			-- tag_list[#tag_list + 1] = tag
+		-- end
+		-- local tags_string = table.concat(tag_list, ", ")
+		-- mod:echo("%s: %s", breed_name, tags_string)
+	-- end
 	if breed.tags.minion then
 		local default_value = false
-		if breed.tags.elite or breed.tags.special then
+		if breed.tags.elite or breed.tags.special or breed.tags.ritualist then
 			default_value = true
 		end
 
@@ -27,8 +36,10 @@ for breed_name, breed in pairs(Breeds) do
 			add(elites, breed_name, default_value)
 		elseif breed.tags.special then
 			add(specials, breed_name, default_value)
-		elseif breed.tags.monster or breed.tags.captain then
+		elseif breed.tags.monster or breed.tags.captain or breed.tags.cultist_captain then
 			add(monsters, breed_name, default_value)
+		elseif breed.tags.ritualist then
+			add(ritualists, breed_name, default_value)
 		end
 	end
 end
@@ -89,6 +100,11 @@ local widgets = {
 		setting_id = "monster_breeds",
 		type = "group",
 		sub_widgets = monsters,
+	},
+	{
+		setting_id = "ritualist_breeds",
+		type = "group",
+		sub_widgets = ritualists,
 	},
 }
 
