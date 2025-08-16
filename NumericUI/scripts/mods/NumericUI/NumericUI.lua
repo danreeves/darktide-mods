101c9a6ad6ebfa48d18dec598fd616cdc1c201a0
Dan Reeves <hey@danreev.es>
2025-08-16 17:18:41 +0100

    make numericui hud recreation safer

--- NumericUI/scripts/mods/NumericUI/NumericUI.lua
+++ NumericUI/scripts/mods/NumericUI/NumericUI.lua
@@ -56,28 +56,40 @@ mod:hook("UIHud", "init", function(func, self, elements, visibility_groups, para
 	return func(self, elements, visibility_groups, params)
 end)
 
--- Taken from Fracticality
 local function recreate_hud()
 	local ui_manager = Managers.ui
-	if ui_manager then
-		local hud = ui_manager._hud
-		if hud then
-			local player_manager = Managers.player
-			local player = player_manager:local_player(1)
-			local peer_id = player:peer_id()
-			local local_player_id = player:local_player_id()
-			local elements = hud._element_definitions
-			local visibility_groups = hud._visibility_groups
 
-			hud:destroy()
-			ui_manager:create_player_hud(peer_id, local_player_id, elements, visibility_groups)
-		end
+	if not ui_manager or not ui_manager._hud then
+		-- UI manager or HUD is not ready, waiting...
+		return false
 	end
+	local player_manager = Managers.player
+	local player = player_manager:local_player(1)
+	if not player then
+		-- Local player is not ready, waiting...
+		return false
+	end
+	local hud = ui_manager._hud
+	local peer_id = player:peer_id()
+	local local_player_id = player:local_player_id()
+	local elements = hud._element_definitions
+	local visibility_groups = hud._visibility_groups
+
+	hud:destroy()
+	ui_manager:create_player_hud(peer_id, local_player_id, elements, visibility_groups)
+	return true
+end
+
+local initialized = false
+mod.on_update = function()
+	if initialized then
+		return
+	end
+	initialized = recreate_hud()
 end
 
 mod.on_all_mods_loaded = function()
-	recreate_hud()
-
+	initialized = false
 	if mod:get("show_medical_crate_radius") then
 		local package_name = "content/levels/training_grounds/missions/mission_tg_basic_combat_01"
 		Managers.package:load(package_name, "NumericUI")
@@ -85,5 +97,5 @@ mod.on_all_mods_loaded = function()
 end
 
 mod.on_setting_changed = function()
-	recreate_hud()
+	initialized = false
 end
