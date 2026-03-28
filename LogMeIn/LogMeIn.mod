b324f2d8397ba6273610d6ef319f23a9343d905d
Dan Reeves <hey@danreev.es>
2026-03-28 16:52:33 +0000

    fix logmein race condition

--- LogMeIn/LogMeIn.mod
+++ LogMeIn/LogMeIn.mod
@@ -9,6 +9,6 @@ return {
 		})
 	end,
 	packages = {},
-	version = "26.02.08-1",
+	version = "26.03.28",
 	mod_id = "15",
 }
--- LogMeIn/scripts/mods/LogMeIn/LogMeIn.lua
+++ LogMeIn/scripts/mods/LogMeIn/LogMeIn.lua
@@ -23,7 +23,8 @@ end)
 mod:hook_safe("MainMenuView", "_set_waiting_for_profiles", function(self, waiting)
 	if mod:get("auto_character_select") and state.first_load and not waiting then
 		cancel()
-		self:_on_play_pressed()
+		mod.pending_main_menu_self = self
+		mod.pending_play_delay = 1
 	end
 	state.first_load = false
 end)
@@ -32,5 +33,19 @@ mod.cancel_auto_character_select = function()
 	if state.first_load then
 		mod:notify("Automatic character selection cancelled")
 		cancel()
+	end
+end
+
+mod.update = function(dt)
+	if mod.pending_play_delay then
+		mod.pending_play_delay = mod.pending_play_delay - dt
+		if mod.pending_play_delay <= 0 then
+			if mod.pending_main_menu_self then
+				mod.pending_main_menu_self:_on_play_pressed()
+				mod.pending_main_menu_self = nil
+				mod.update = nil
+			end
+			mod.pending_play_delay = nil
+		end
 	end
 end
