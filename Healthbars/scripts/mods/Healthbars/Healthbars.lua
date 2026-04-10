0b0c27397612de77564bbe3d39afc9b7a0dedabb
Ludwig Haas <ludwig@microtask.de>
2026-04-10 19:56:34 +0200

    fix: resync custom healthbar markers and limit vanilla suppression to Psykhanium
    - add HUD world marker backfill for already existing enemy units
    - reset custom marker cache when world marker HUD initializes
    - prevent duplicate custom marker registration within the same HUD lifecycle
    - keep vanilla damage indicator suppression scoped to Psykhanium only
    - harden healthbar eligibility checks against missing unit data extensions

--- Healthbars/scripts/mods/Healthbars/Healthbars.lua
+++ Healthbars/scripts/mods/Healthbars/Healthbars.lua
@@ -27,9 +27,23 @@ local WARPFIRE_COLOR_OPTIONS = {
 	warpfire_color_option_five = { 255, 138, 43, 226 },
 }
 
+local ScriptUnit = ScriptUnit
+local Managers = Managers
+local ALIVE = ALIVE
+local pairs = pairs
+local setmetatable = setmetatable
+local type = type
+local string_match = string.match
+
 local function copy_color(color)
 	return { color[1], color[2], color[3], color[4] }
 end
+
+local function new_marker_cache()
+	return setmetatable({}, { __mode = "k" })
+end
+
+mod._custom_marker_units = mod._custom_marker_units or new_marker_cache()
 
 local function refresh_colors()
 	local warpfire_key = mod:get("warpfire_color_option") or "warpfire_color_option_three"
@@ -60,15 +74,11 @@ function mod.on_all_mods_loaded()
 	load_package("packages/ui/views/inventory_background_view/inventory_background_view")
 end
 
-mod:hook_safe("HudElementWorldMarkers", "init", function(self)
-	self._marker_templates[MarkerTemplate.name] = MarkerTemplate
-end)
-
 local show = {}
 
 local function get_toggles()
 	for breed_name in pairs(Breeds) do
-		if string.match(breed_name, "mutator") then
+		if string_match(breed_name, "mutator") then
 			show[breed_name] = mod:get((breed_name):gsub("_mutator", ""))
 		else
 			show[breed_name] = mod:get(breed_name)
@@ -76,31 +86,118 @@ local function get_toggles()
 	end
 end
 
+local function is_psykhanium()
+	local game_mode_manager = Managers.state and Managers.state.game_mode
+	return game_mode_manager and game_mode_manager:game_mode_name() == "shooting_range"
+end
+
+local function should_enable_healthbar(unit)
+	if not ALIVE[unit] then
+		return false
+	end
+
+	local unit_data_extension = ScriptUnit.has_extension(unit, "unit_data_system")
+	if not unit_data_extension then
+		return false
+	end
+
+	local breed = unit_data_extension:breed()
+	return breed and show[breed.name] == true or false
+end
+
+local function add_custom_healthbar_marker(unit)
+	if not should_enable_healthbar(unit) then
+		return false
+	end
+
+	local event_manager = Managers.event
+	if not event_manager then
+		return false
+	end
+
+	local custom_marker_units = mod._custom_marker_units
+	if custom_marker_units[unit] then
+		return false
+	end
+
+	custom_marker_units[unit] = true
+	event_manager:trigger("add_world_marker_unit", MarkerTemplate.name, unit)
+
+	return true
+end
+
+local function visit_units_from_container(container, seen_units)
+	if not container then
+		return 0
+	end
+
+	local count = 0
+
+	for key, value in pairs(container) do
+		local unit = nil
+
+		if ALIVE[key] then
+			unit = key
+		elseif type(value) == "table" then
+			local value_unit = value._unit
+			if ALIVE[value_unit] then
+				unit = value_unit
+			end
+		end
+
+		if unit and not seen_units[unit] then
+			seen_units[unit] = true
+
+			if add_custom_healthbar_marker(unit) then
+				count = count + 1
+			end
+		end
+	end
+
+	return count
+end
+
+local function resync_existing_healthbars()
+	local state_manager = Managers.state
+	local extension_manager = state_manager and state_manager.extension
+	if not extension_manager then
+		return 0
+	end
+
+	local health_system = extension_manager:system("health_system")
+	if not health_system then
+		return 0
+	end
+
+	local seen_units = {}
+	local added = 0
+
+	added = added + visit_units_from_container(health_system._unit_to_extension_map, seen_units)
+	added = added + visit_units_from_container(health_system._health_extensions, seen_units)
+	added = added + visit_units_from_container(health_system._extensions, seen_units)
+
+	return added
+end
+
 get_toggles()
 
 mod.on_setting_changed = function()
 	get_toggles()
 	refresh_colors()
+	resync_existing_healthbars()
 end
 
-local function should_enable_healthbar(unit)
-	local unit_data_extension = ScriptUnit.extension(unit, "unit_data_system")
-	local breed = unit_data_extension:breed()
-
-	if show[breed.name] then
-		return true
-	end
-
-	return false
-end
+mod:hook_safe("HudElementWorldMarkers", "init", function(self)
+	self._marker_templates[MarkerTemplate.name] = MarkerTemplate
+	mod._custom_marker_units = new_marker_cache()
+	resync_existing_healthbars()
+end)
 
 mod:hook_safe(
 	"HealthExtension",
 	"init",
 	function(_self, _extension_init_context, unit, _extension_init_data, _game_object_data)
-		if should_enable_healthbar(unit) then
-			Managers.event:trigger("add_world_marker_unit", MarkerTemplate.name, unit)
-		end
+		add_custom_healthbar_marker(unit)
 	end
 )
 
@@ -116,19 +213,13 @@ mod:hook_safe(
 		self.was_hit_by_critical_hit_this_render_frame = HealthExtension.was_hit_by_critical_hit_this_render_frame
 
 		-- Set has a healthbar
-		if should_enable_healthbar(unit) then
-			Managers.event:trigger("add_world_marker_unit", MarkerTemplate.name, unit)
-		end
+		add_custom_healthbar_marker(unit)
 	end
 )
 
 mod:hook("HudElementWorldMarkers", "event_add_world_marker_unit", function(func, self, marker_type, unit, callback, data)
-	if marker_type == "damage_indicator" and ALIVE[unit] then
-		local game_mode_name = Managers.state.game_mode and Managers.state.game_mode:game_mode_name()
-
-		if game_mode_name == "shooting_range" and should_enable_healthbar(unit) then
-			return
-		end
+	if marker_type == "damage_indicator" and is_psykhanium() and should_enable_healthbar(unit) then
+		return
 	end
 
 	return func(self, marker_type, unit, callback, data)
