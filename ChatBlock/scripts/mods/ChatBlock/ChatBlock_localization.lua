089ccb183d0e5da1e71110c4fb3c590e9ae03acc
SyuanTsai <carsun00@gmail.com>
2026-07-10 01:25:40 +0800

    rollback ru

--- ChatBlock/scripts/mods/ChatBlock/ChatBlock_localization.lua
+++ ChatBlock/scripts/mods/ChatBlock/ChatBlock_localization.lua
@@ -8,8 +8,8 @@ return {
 	mod_description = {
 		en = "Automatically hold block while chatting, opening the Steam overlay, or while the game is not focussed",
 		["zh-cn"] = "在打开聊天、打开 Steam 游戏内界面或者切换到其他窗口时自动格挡",
-		ru =
-		"Chat Block - Автоматически удерживается блок во время использования чата, при открытии оверлея Steam или когда игра не находится в фокусе.",
+ 		ru =
+ 		"Chat Block - Автоматически удерживается блок во время использования чата, при открытии оверлея Steam или когда игра не находится в фокусе.",
 		["zh-tw"] = "聊天、開啟 Steam 內嵌介面，或焦點不在遊戲視窗時自動維持格擋",
 	},
 }
