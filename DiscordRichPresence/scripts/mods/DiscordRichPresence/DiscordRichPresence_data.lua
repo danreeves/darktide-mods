0ed64bc284cec8cc4c1e0aef5ec193f3a7ed86b2
Dan Reeves <hey@danreev.es>
2026-09-15 23:42:23 +0100

    delete rtc and discord rich presence

--- DiscordRichPresence/scripts/mods/DiscordRichPresence/DiscordRichPresence_data.lua
+++
@@ -1,7 +0,0 @@
-local mod = get_mod("DiscordRichPresence")
-
-return {
-	name = mod:localize("mod_name"),
-	description = mod:localize("mod_description"),
-	is_togglable = false,
-}
--- rtc/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# rtc Changelog
-
-## 0.0.0
-
-Empty changelog.
